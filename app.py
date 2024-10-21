a2ef545e59e2ceb263f8a90c4c7ffdfddddc2b66
Lázaro Galdino <132415717+LazaroJoseph@users.noreply.github.com>
2024-10-21 10:17:31 -0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +8,16 @@ app = Flask(__name__)
 def index():
     return "<h1>Olá <br> Mundo!</h1>"
 
-@app.route("/tiktok/<username>")
-def aluno (username):
-    return f'<h1>{username}</h1>'
+@app.route("/inicio")
+def usuario ():
+    return render_template('formulario.html')
+
+@app.route("/envio", methods=['POST'])
+def envioForms():
+    nombre=request.form['username']
+    password=request.form['password']
+    if password == '123':
+        return render_template('username.html', n=nombre)
+    else:
+        return render_template('formulario.html')
+    
